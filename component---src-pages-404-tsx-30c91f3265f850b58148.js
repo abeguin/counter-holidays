f78c8e05617d21ed4f4467e0a273d2e2e57de8f4
Arnaud Béguin <abeguin@pictet.com>
2022-12-28 11:16:36 +0100

    Updates

---
+++ component---src-pages-404-tsx-30c91f3265f850b58148.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcounter=self.webpackChunkcounter||[]).push([[218],{2513:function(e,t,n){n.r(t);var u=n(7294),l=n(4842),r=n(719);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-30c91f3265f850b58148.js.map
